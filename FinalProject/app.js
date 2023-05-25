50b707acc968321f46e68dede514e7f406f238a9
iseejunaid <junaidnadeem266@gmail.com>
2023-05-25 16:54:20 +0500

    working express

--- FinalProject/app.js
+++ FinalProject/app.js
@@ -5,7 +5,6 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var destinationRouter = require('./routes/destinations');
 
 var app = express();
 
@@ -18,11 +17,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use('/', indexRouter);
-app.get('/destinations', (req, res) => {
-  res.render('destinations');  
-});
 
+app.use('/', indexRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
