76395ec0531a981dfcb96a2de557f34fc8b325e9
iseejunaid <junaidnadeem266@gmail.com>
2023-06-07 01:50:03 +0500

    made routes and apis

--- FinalProject/routes/destinations.js
+++ FinalProject/routes/destinations.js
@@ -2,7 +2,9 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('home');
+router.get('/', (req, res) => {
+  res.render('destinations');  
 });
+
+
 module.exports = router;
